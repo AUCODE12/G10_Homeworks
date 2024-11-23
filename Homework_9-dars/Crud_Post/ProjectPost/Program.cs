64d881db0223a1dcbe58c5ce1d0925fda8e87d18
Ahmadjon Qudratov <120676674+AUCODE12@users.noreply.github.com>
2024-11-23 12:30:18 +0500

    Update

--- Homework_9-dars/Crud_Post/ProjectPost/Program.cs
+++ Homework_9-dars/Crud_Post/ProjectPost/Program.cs
@@ -1,10 +1,60 @@
-﻿namespace ProjectPost
+﻿using ProjectPost.Sevices;
+
+namespace ProjectPost
 {
     internal class Program
     {
         static void Main(string[] args)
+        {  
+        }
+        
+        public static void RunApp()
         {
-            Console.WriteLine("Hello, World!");
+            var postServce = new PostService();
+
+            while (true)
+            {
+                Console.WriteLine("1. Add post");
+                Console.WriteLine("2. Update post");
+                Console.WriteLine("3. Delete post");
+                Console.WriteLine("4. Get post");
+                Console.WriteLine("5. GetByID post");
+                Console.WriteLine("6. Add like post");
+                Console.WriteLine("7. Add Comment post");
+                Console.WriteLine("8. Add Viewer post");
+                Console.WriteLine();
+                Console.Write("Enter Choose: ");
+                var option = int.Parse(Console.ReadLine());
+
+                switch(option)
+                {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+
+                        break;
+                }
+
+            }
         }
     }
 }
